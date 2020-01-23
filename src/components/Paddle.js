650fd24f4f42ad47aef11885f1b3ca25e57a0c38
Brian Bartholomew <me@bcbrian.com>
2020-01-22 20:50:49 -0600

    mouse movement captured
    still need to apply it.

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Paddle.css";
 
 export default function Paddle() {
+  function handleMouse(e) {
+    console.log(e.target, e.x, e.y, e.movementX, e.movementY);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+    return () => window.removeEventListener("mousemove", handleMouse);
+  }, []);
   return <div className="paddle" />;
 }
